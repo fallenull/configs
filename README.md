4b0b87289e3e315c9f7a3be2e99b58112ca8fa61
Akarumei <akarumei@protonmail.com>
2020-12-19 17:47:35 +0500

    Add WesomeWM config

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# configs
+configs
+=======
 There are my own configs
+
+awesomewm
+---------
+for applications by right-click:
+`xdg_menu --format awesome --root-menu /etc/xdg/menus/arch-applications.menu >~/.config/awesome/archmenu.lua`
+
+akmcnf tool
+-----------
+just start it and it will install these configs
---
+++ archmenu.lua
@@ -0,0 +1,118 @@
+ local menubeceed89e397495003f6ff5a4d895d66 = {
+     {"Audacity", "env UBUNTU_MENUPROXY=0 audacity ", "/usr/share/icons/hicolor/16x16/apps/audacity.png" },
+     {"Cheese", "cheese"},
+     {"OBS Studio", "obs", "/usr/share/icons/hicolor/256x256/apps/com.obsproject.Studio.png" },
+     {"Qt V4L2 test Utility", "qv4l2", "/usr/share/icons/hicolor/16x16/apps/qv4l2.png" },
+     {"Qt V4L2 video capture utility", "qvidcap", "/usr/share/icons/hicolor/16x16/apps/qvidcap.png" },
+     {"Видео", "totem "},
+     {"Музыка", "gnome-music"},
+     {"Регулятор громкости PulseAudio", "pavucontrol"},
+ }
+
+ local menu9a1dcda9efe2bfc3963e75f98cc1ffd5 = {
+     {"GNU Image Manipulation Program", "gimp-2.10 ", "/usr/share/icons/hicolor/16x16/apps/gimp.png" },
+     {"Krita", "krita ", "/usr/share/icons/hicolor/16x16/apps/krita.png" },
+     {"LibreOffice Draw", "libreoffice --draw ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-draw.png" },
+     {"Slideshow", "/usr/bin/slideshow", "///usr/share/racket/slideshow-exe-icon.png" },
+     {"Программа просмотра изображений «Глаз MATE»", "eom ", "/usr/share/icons/hicolor/16x16/apps/eom.png" },
+     {"Просмотр документов", "evince "},
+     {"Просмотр изображений", "eog "},
+     {"Сканер документов", "simple-scan"},
+ }
+
+ local menu25385e702800d748c19cf6ea4f5811ec = {
+     {"Chromium", "/usr/bin/chromium ", "/usr/share/icons/hicolor/16x16/apps/chromium.png" },
+     {"Discord", "/usr/bin/discord", "/usr/share/pixmaps/discord.png" },
+     {"Element", "/usr/bin/element-desktop ", "/usr/share/icons/hicolor/16x16/apps/io.element.Element.png" },
+     {"Firefox", "/usr/lib/firefox/firefox ", "/usr/share/icons/hicolor/16x16/apps/firefox.png" },
+     {"Gajim", "gajim "},
+     {"Kotatogram Desktop", "kotatogram-desktop -- ", "/usr/share/icons/hicolor/16x16/apps/kotatogram.png" },
+     {"Psi+", "psi-plus ", "/usr/share/icons/hicolor/16x16/apps/psi-plus.png" },
+     {"Telegram Desktop", "telegram-desktop -- ", "/usr/share/icons/hicolor/16x16/apps/telegram.png" },
+     {"Thunderbird", "/usr/lib/thunderbird/thunderbird ", "/usr/share/icons/hicolor/16x16/apps/thunderbird.png" },
+     {"qBittorrent", "qbittorrent ", "/usr/share/icons/hicolor/16x16/apps/qbittorrent.png" },
+     {"Веб-браузер", "epiphany "},
+     {"Навигатор Avahi по серверам SSH", "/usr/bin/bssh"},
+     {"Навигатор Avahi по серверам VNC", "/usr/bin/bvnc"},
+ }
+
+ local menud26c1170c5110a4264a8a8a89ec20529 = {
+     {"DrRacket", "drracket -singleInstance "},
+     {"LibreOffice Math", "libreoffice --math ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-math.png" },
+ }
+
+ local menu8caf0567988f6898bf53e8c848c6f7bc = {
+     {"LibreOffice", "libreoffice ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-startcenter.png" },
+     {"LibreOffice Base", "libreoffice --base ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-base.png" },
+     {"LibreOffice Calc", "libreoffice --calc ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-calc.png" },
+     {"LibreOffice Draw", "libreoffice --draw ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-draw.png" },
+     {"LibreOffice Impress", "libreoffice --impress ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-impress.png" },
+     {"LibreOffice Math", "libreoffice --math ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-math.png" },
+     {"LibreOffice Writer", "libreoffice --writer ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-writer.png" },
+     {"Календарь", "gnome-calendar"},
+     {"Просмотр документов", "evince "},
+     {"Просмотрщик документов Atril", "atril ", "/usr/share/icons/hicolor/16x16/apps/atril.png" },
+ }
+
+ local menu8b25bc71fce7a929a6124e9354c07cdb = {
+     {"CMake", "cmake-gui ", "/usr/share/icons/hicolor/32x32/apps/CMakeSetup.png" },
+     {"DrRacket", "drracket -singleInstance "},
+     {"Electron", "electron ", "/usr/share/pixmaps/electron.png" },
+     {"Emacs", "emacs ", "/usr/share/icons/hicolor/16x16/apps/emacs.png" },
+     {"GitKraken", "gitkraken ", "/usr/share/pixmaps/gitkraken.png" },
+     {"ipython", "xterm -e ipython", "/usr/share/pixmaps/ipython.png" },
+     {"Боксы", "gnome-boxes "},
+ }
+
+ local menu13155df48f218a078bb8bdc9649e5b20 = {
+     {"Alacritty", "alacritty", "/usr/share/pixmaps/Alacritty.svg" },
+     {"Hardware Locality lstopo", "lstopo"},
+     {"Htop", "xterm -e htop", "/usr/share/pixmaps/htop.png" },
+     {"Parental Controls", "malcontent-control"},
+     {"ranger", "xterm -e ranger"},
+     {"Анализатор использования дисков", "baobab "},
+     {"Боксы", "gnome-boxes "},
+     {"Журналы", "gnome-logs"},
+     {"Обозреватель Avahi Zeroconf", "/usr/bin/avahi-discover"},
+     {"Просмотрщик цветовых профилей", "gcm-viewer", "/usr/share/icons/hicolor/16x16/apps/gnome-color-manager.png" },
+     {"Редактор dconf", "dconf-editor", "/usr/share/icons/hicolor/16x16/apps/ca.desrt.dconf-editor.png" },
+     {"Системный монитор", "gnome-system-monitor"},
+     {"Терминал", "gnome-terminal"},
+     {"Терминал MATE", "mate-terminal"},
+     {"Центр приложений", "gnome-software "},
+ }
+
+ local menu91af6ed4e0be6a48ab48ec8b6e768568 = {
+     {"GVim", "gvim -f ", "/usr/share/icons/hicolor/48x48/apps/gvim.png" },
+     {"Pluma", "pluma "},
+     {"Vim", "xterm -e vim ", "/usr/share/icons/hicolor/48x48/apps/gvim.png" },
+     {"compton", "compton", "/usr/share/icons/hicolor/48x48/apps/compton.png" },
+     {"ipython", "xterm -e ipython", "/usr/share/pixmaps/ipython.png" },
+     {"picom", "picom"},
+     {"Диски", "gnome-disks"},
+     {"Калькулятор", "gnome-calculator"},
+     {"Калькулятор MATE", "mate-calc"},
+     {"Карты", "gapplication launch org.gnome.Maps "},
+     {"Менеджер архивов", "file-roller "},
+     {"Менеджер архивов Engrampa", "engrampa ", "/usr/share/icons/hicolor/16x16/apps/engrampa.png" },
+     {"Погода", "gapplication launch org.gnome.Weather"},
+     {"Программа просмотра шрифтов MATE", "mate-font-viewer "},
+     {"Символы", "/usr/bin/gnome-characters"},
+     {"Снимок экрана", "gnome-screenshot --interactive"},
+     {"Текстовый редактор", "gedit "},
+     {"Файлы", "nautilus --new-window "},
+     {"Часы", "gnome-clocks"},
+     {"Шрифты", "gnome-font-viewer "},
+ }
+
+xdgmenu = {
+    {"Аудио и видео", menubeceed89e397495003f6ff5a4d895d66},
+    {"Графика", menu9a1dcda9efe2bfc3963e75f98cc1ffd5},
+    {"Интернет", menu25385e702800d748c19cf6ea4f5811ec},
+    {"Образовательные", menud26c1170c5110a4264a8a8a89ec20529},
+    {"Офис", menu8caf0567988f6898bf53e8c848c6f7bc},
+    {"Программирование", menu8b25bc71fce7a929a6124e9354c07cdb},
+    {"Системные", menu13155df48f218a078bb8bdc9649e5b20},
+    {"Стандартные", menu91af6ed4e0be6a48ab48ec8b6e768568},
+}
+
